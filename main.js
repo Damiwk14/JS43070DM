f17b11384dadbb7af785886c980642c7736399d2
DamianMattos <mattosdamianezequiel@hotmail.com>
2023-05-01 12:28:38 -0300

    primer entrega v2

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-
+function simuladorPrestamo(){
 alert(`Bienvenido al simulador de prestamos!`)
 let interes = 1.05
 let cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las que desearía devolver el préstamo"));
     
 while (cuotas == null || /\D/.test(cuotas) || cuotas == "") {
-    cuotas = prompt("Ingrese un valor numérico: ");
+    cuotas = prompt("Ingrese un valor numérico para las cuotas: ");
 };
 
 let monto = parseInt(prompt("ingrese el monto necesario"));
     
 while (monto == null || /\D/.test(monto) || monto == "") {
-    monto = prompt("Ingrese un valor numérico: ");
+    monto = prompt("Ingrese un valor numérico para el monto del prestamo: ");
 };
 
 for(let i = 1; i <= cuotas; i++){
@@ -18,3 +18,8 @@ for(let i = 1; i <= cuotas; i++){
     alert(`La cuota ${i} es ${resultado}`);
 
 }
+}
+
+simuladorPrestamo()
+
+// se ejecuta el proceso al invocar la función declarada
